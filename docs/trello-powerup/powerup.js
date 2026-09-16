c7d215f173159b83aa2860cdebd5c6cedf9c24d8
teklayty <teklay.yifter@sheffield.cityofsanctuary.org>
2026-09-16 09:27:39 +0100

    Fix Trello Mark Read authorization flow

--- docs/trello-powerup/powerup.js
+++ docs/trello-powerup/powerup.js
@@ -1,32 +1,21 @@
 (function () {
   "use strict";
 
-  const POWERUP_APP_NAME = "WhatsApp New Message - Mark Read";
-  const POWERUP_APP_KEY = "99fb195773ddc1e3bfbd79de31dd8647";
-
-  function markReadButton(t) {
-    return t.popup({
-      title: "Mark Read",
-      url: "./mark-read.html",
-      height: 180
-    });
-  }
-
-  window.TrelloPowerUp.initialize(
-    {
-      "card-buttons": function () {
-        return [
-          {
-            text: "Mark Read",
-            condition: "edit",
-            callback: markReadButton
+  window.TrelloPowerUp.initialize({
+    "card-buttons": function () {
+      return [
+        {
+          text: "Mark Read",
+          condition: "edit",
+          callback: function (t) {
+            return t.popup({
+              title: "Mark Read",
+              url: "./mark-read.html",
+              height: 200
+            });
           }
-        ];
-      }
-    },
-    {
-      appKey: POWERUP_APP_KEY,
-      appName: POWERUP_APP_NAME
+        }
+      ];
     }
-  );
+  });
 })();
