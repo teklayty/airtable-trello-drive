906e0395a22f897d81baacb28d9b1121d883170f
teklayty <teklay.yifter@sheffield.cityofsanctuary.org>
2026-09-16 11:41:31 +0100

    Implement Mark Read authorization and label removal

--- docs/trello-powerup/powerup.js
+++ docs/trello-powerup/powerup.js
@@ -1,18 +1,20 @@
 (function () {
   "use strict";
 
+  function showMarkRead(t) {
+    return t.popup({
+      title: "Mark Read",
+      url: "./mark-read.html",
+      height: 220
+    });
+  }
+
   window.TrelloPowerUp.initialize({
     "card-buttons": function () {
       return [
         {
           text: "Mark Read",
-          callback: function (t) {
-            return t.popup({
-              title: "Mark Read",
-              url: "./mark-read.html",
-              height: 220
-            });
-          }
+          callback: showMarkRead
         }
       ];
     }
